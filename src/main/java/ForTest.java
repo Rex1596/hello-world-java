55776f8ffd330ec1f5d3d4ad5c6fffd0d6b165b8
Rex1596 <a15626069093@aliyun.com>
2021-05-11 16:24:14 +0800

    for 循环提交

---
+++ src/main/java/ForTest.java
@@ -0,0 +1,12 @@
+/**
+ * Author lzw
+ * Create 2021/5/11 16:22
+ * Description for Test
+ */
+public class ForTest {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
