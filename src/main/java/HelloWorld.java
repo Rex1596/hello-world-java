1d836a1e012eaff11d1064e442154464064505d2
Rex1596 <a15626069093@aliyun.com>
2021-05-11 15:31:21 +0800

    commit HelloWorld.java

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Author lzw
+ * Create 2021/5/11 15:07
+ * Description hello world by java
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world !!!");
+    }
+}
