13a3b5035a0a92c79453d50efc72592aeeadec6f
lzw <a15626069093@aliyun.com>
2023-05-05 15:15:44 +0800

    更新依赖包

--- src/main/java/HelloWorld1.java
+++ src/main/java/HelloWorld1.java
@@ -3,7 +3,7 @@
  * Create 2021/5/11 15:07
  * Description hello world by java
  */
-public class HelloWorld {
+public class HelloWorld1 {
     public static void main(String[] args) {
         System.out.println("Hello world ！！！");
     }
